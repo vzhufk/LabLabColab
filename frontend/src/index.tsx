1965c651d3506dc18e8c998d0329bb3c70fb19a5
Vadym_Zhufiak <Vadym_Zhufiak@EPAM.com>
2023-09-21 11:28:44 +0300

    FE: add basename for gh pages

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='LabLabColab'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
